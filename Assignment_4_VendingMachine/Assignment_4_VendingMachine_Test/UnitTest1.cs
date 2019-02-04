c144889111df035f1f7254b318b9a54d669944e9
Fady Ksho <fadyksho@gmail.com>
2019-02-04 13:50:33 +0100

    Initialize Assignment 4 Vending Machine

---
+++ Assignment_4_VendingMachine/Assignment_4_VendingMachine_Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment_4_VendingMachine_Test
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
